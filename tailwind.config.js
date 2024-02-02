80021ad52607d720702ce6f6e0299d3dc019d0e4
Rizalramzi <rizalramzi0@gmail.com>
2024-02-02 07:34:26 +0700

    feat: add PeminjamanBarang pages and auth pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,23 @@ module.exports = {
         'natural-6': '#C2C2C2',
         'natural-7': '#E0E0E0',
         'natural-4': '#757575',
+        'natural-1' : '#0A0A0A',
+        'natural-5' : '#9E9E9E',
+        'natural-20' : '#F5F5F5',
         'mainbg' : '#FDEAEA',
-        'main': '#E4252C'
+        'main': '#E4252C',
+        'main_hover' : '#FF373F',
       },
       boxShadow: {
         'sidebar': '9px 4px 78px 0px rgba(0, 0, 0, 0.05);',
         'formBarang': '0px 0px 55.7px 0px rgba(0, 0, 0, 0.07);',
       },
       screens: {
-        'hp': '375px',
+        'hp': '319px',
         'tablet': '768px',  
         'laptop': '1024px',  
-        'desktop': '1400',
+        'desktop': '1280px',
+        'desktop-lg': '1440px',
       },
       gridTemplateColumns: {
         '2columns': '280px 1fr',
